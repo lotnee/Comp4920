cf7cd5d486803e507619462ca2dba50f94f37981
wickwickthedog <htsy97@icloud.com>
2019-11-21 23:24:30 +1100

    update event model

--- app/models/events.py
+++ app/models/events.py
@@ -3,19 +3,20 @@ from app.database import DB
 
 class Event(object):
 
-    def __init__(self,name,description,start,end,host,invitees):
+    def __init__(self,name,description,start,end,host,invitees, pictureDir, private):
         self.name = name
         self.description = description
         self.start = start
         self.end = end
         self.host = host
         self.invitees = invitees
+        self.pictureDir = pictureDir
+        self.private = private
 
     def insert(self,userEmail):
-        hehe = DB.insert(collection='Events', data=self.json())
-        DB.update_one(collection='Profile',filter={'email':userEmail}, data = {'$push': {'events':hehe.inserted_id}})
-        DB.update_one(collection = "Events", filter ={'_id':hehe.inserted_id}, data = {'$push': {"invitees": {"email": userEmail, "status": "going"}}})
-        return hehe
+        event = DB.insert(collection='Events', data=self.json())
+        DB.update_one(collection='Profile',filter={'email':userEmail}, data = {'$push': {'events':event.inserted_id}})
+        DB.update_one(collection = "Events", filter ={'_id':event.inserted_id}, data = {'$push': {"invitees": {"email": userEmail, "status": "going"}}})
 
     def addProfile(self, newProfile,eventId ):
         DB.update_one(collection='Profile', filter = {'_id':eventId}, data = {'$push':{'invitees':newProfile}})
@@ -28,5 +29,7 @@ class Event(object):
         	'start': self.start,
         	'end': self.end,
             'host':self.host,
-            'invitees':self.invitees
+            'invitees':self.invitees,
+            'pictureDir': self.pictureDir,
+            'private': self.private
         }
