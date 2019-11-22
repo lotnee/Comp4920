27e83e7e5886eb004fe6283a798d2c23e43aaffa
piescotch <z5114240@unsw.edu.au>
2019-11-22 16:13:13 +1100

    Replace host ids with host names in search page

--- app/controllers/searchRoutes.py
+++ app/controllers/searchRoutes.py
@@ -20,14 +20,20 @@ def search():
 	# since the host of an event can change, or the name of the host can
 	# change, we need to look up event hosts by profile id instead of by
 	# name
-	matched_profile_ids = [profile['_id'] for profile in matched_profiles]
+	matched_profile_info = {profile['_id'] : (profile['firstName'], profile['lastName']) 
+				for profile in matched_profiles}
 
+	import pdb
 	DB.createIndex(collection="Events", query=[("name", "text"), ("description", "text")], name='event_search')
 	DB.createIndex(collection="Events", query=[("host", 1)], name='event_host')
 	matched_events = list(DB.find(collection='Events', query= {
 		'$or':[{'$text': {'$search': request.args['query']}},
-			{'host': {'$in': matched_profile_ids}}]
+			{'host': {'$in': list(matched_profile_info.keys())}}]
 		}))
+
+	for event in matched_events:
+		(firstname, lastname) = matched_profile_info[event['host']]
+		event['host'] = '{} {}'.format(firstname,lastname)
 
 	return render_template('search.html', title='Search Results',
 		users=matched_profiles, events=matched_events, query=request.args['query'])
