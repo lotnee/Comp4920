908a1fa858b3f9a9989911c95d42438f0cbc523f
Anna Ung <z5115212@student.unsw.edu.au>
2019-11-23 21:45:27 +1100

    comment for rodney

--- app/controllers/profileRoutes.py
+++ app/controllers/profileRoutes.py
@@ -28,6 +28,7 @@ def dashboard():
 	me = DB.find_one(collection="Profile", query={"email": current_user.email})
 	incoming = DB.find(collection="Profile", query={"friends": {"$elemMatch": {"email": current_user.email, "status": "pending"}}})
 	requests = get_cursor(cursor_obj=incoming, key="friends", subkey="email", subkey2="status", query=current_user.email, query2="pending")
+	# and heeerreeee rodney
 	if DB.find_one(collection="Profile", query={"email":current_user.email, "events": {"$ne" : []}}):
 		eventList = DB.find(collection="Profile", query={"email":current_user.email, "events": {"$ne" : []}})
 		print(eventList[0]['events'])
