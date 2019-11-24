19777e2759482d237ad2082703ae2ddd9889096c
wickwickthedog <htsy97@icloud.com>
2019-11-24 11:12:51 +1100

    fixed incoming friend request in dashboard

--- app/controllers/profileRoutes.py
+++ app/controllers/profileRoutes.py
@@ -33,7 +33,7 @@ def dashboard():
 		return redirect(url_for('edit_profile'))
 	users = list(DB.find_all(collection="Profile"))
 	# me = DB.find_one(collection="Profile", query={"email": current_user.email})
-	incoming = DB.find(collection="Profile", query={"friends": {"$elemMatch": {"email": current_user.email, "status": "pending"}}})
+	incoming = DB.find(collection="Profile", query={"friends": {"$elemMatch": {"friend_id": user['_id'], "status": "pending"}}})
 	requests = get_cursor(cursor_obj=incoming, key="friends", subkey="friend_id", subkey2="status", query=user['_id'], query2="pending")
 	
 	allEvents = []
